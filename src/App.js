d04d6948a3430cc2b85fb63acd3d7184cf91ad9d
Chandler Barlow <chandlerbarlow@Chandlers-MacBook-Pro.local>
2019-06-28 18:41:23 +0900

    Changes to handling "Business Component"
    Changed the way the business component was handled. Now the business information is passed through props.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BuisnessList from './components/BusinessList/BusinessList.js';
+import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
-
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+let businesses = [
+business,
+business,
+business,
+business,
+business,
+business,
+];
 function App() {
   return (
   	<div className = "app">
   	<h1>ravenous</h1>
-  	<BuisnessList/>
+  	<BusinessList businesses = {businesses}/>
   	<SearchBar/>
   	</div>
   	);
